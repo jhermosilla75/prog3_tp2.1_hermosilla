971a140fad421c5d586b4fda2f38b877097074fd
Javier Hermosilla <jvrhermosilla@gmail.com>
2024-06-29 18:05:56 -0300

    listo para entregar

--- ejercicio3/converter.js
+++ ejercicio3/converter.js
@@ -6,49 +6,79 @@ class Currency {
 }
 
 class CurrencyConverter {
-    constructor(apiUrl) {
+    constructor(apiUrl, currencies = []) {
         this.apiUrl = apiUrl;
-        this.currencies = [];
+        this.currencies = currencies;
+
     }
 
     async getCurrencies() {
-        try {
-            const response = await fetch(`${this.apiUrl}/currencies`);
-            if (response.ok){
-                const currencies = await response.json()
-                Object.entries(currencies).forEach(([clave, valor]) => {
-                    let currency = new Currency(clave, valor)
-                    this.currencies.push(currency)                
-                });
-                console.log(this.currencies)
-            }
-        } catch (error) {
-            console.log("error",error.message);
-        }
+        try{
+            const response = await fetch(`${this.apiUrl}/currencies`)
+            const monedas = await response.json();
+            Object.entries(monedas).forEach(([codigo, nombre]) => {
+                let tipoMoneda = new Currency(codigo, nombre);
+                this.currencies.push(tipoMoneda);
+            });
 
+            
+
+        } catch(error){
+            console.error(error);
+        }
+                
     }
+    getYesterdayDate(){
+        const today = new Date();
+        today.setDate(today.getDate() - 1);
+    
+        const year = today.getFullYear();
+        const month = String(today.getMonth() + 1).padStart(2, '0');
+        const day = String(today.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    }
+    
+    
 
     async convertCurrency(amount, fromCurrency, toCurrency) {
-        if(fromCurrency == toCurrency){
-            return parseFloat(amount); 
+        if (fromCurrency == toCurrency || amount <= 0){
+            return parseFloat(amount);
         }
-        try {
+        try{
             const response = await fetch(`${this.apiUrl}/latest?amount=${amount}&from=${fromCurrency.code}&to=${toCurrency.code}`);
             if (response.ok){
-                const convertida = await response.json();
-                return convertida.rates[toCurrency.code];
+            const convertida = await response.json();
+            return convertida.rates[toCurrency.code];
             }
-        } catch (error) {
-            console.log(error.message);
-            return null;
             
+        } catch(error){
+                console.log(error);
+        }
+        
+    }
+
+    async convertCurrency1(amount, fromCurrency, toCurrency){
+        try{
+            const ayer = this.getYesterdayDate();
+            //console.log(ayer);
+            const response = await fetch(`${this.apiUrl}/${ayer}?amount=${amount}&from=${fromCurrency.code}&to=${toCurrency.code}`);
+            if (response.ok){
+            const convertida = await response.json();
+            console.log(convertida.rates);
+            return convertida.rates[toCurrency.code];
+            }
+
+        } catch(error){
+            console.log(error);
         }
     }
+
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
     const form = document.getElementById("conversion-form");
     const resultDiv = document.getElementById("result");
+    const resultDiv1 = document.getElementById("result1");
     const fromCurrencySelect = document.getElementById("from-currency");
     const toCurrencySelect = document.getElementById("to-currency");
 
@@ -75,13 +105,29 @@ document.addEventListener("DOMContentLoaded", async () => {
             toCurrency
         );
 
-        if (convertedAmount !== null && !isNaN(convertedAmount)) {
+        if (convertedAmount !== null && !isNaN(convertedAmount) && convertedAmount > 0) {
             resultDiv.textContent = `${amount} ${
                 fromCurrency.code
-            } son ${convertedAmount.toFixed(2)} ${toCurrency.code} y aqui va la adicional`;
+            } son ${convertedAmount.toFixed(2)} ${toCurrency.code}`;
         } else {
             resultDiv.textContent = "Error al realizar la conversión.";
         }
+
+
+        const convertedAmount1 = await converter.convertCurrency1(
+            amount,
+            fromCurrency,
+            toCurrency
+        );
+
+        if (convertedAmount1 !== null && !isNaN(convertedAmount1) && convertedAmount1 > 0) {
+            resultDiv1.textContent = `AYER ${amount} ${fromCurrency.code} son ${convertedAmount1.toFixed(2)} ${toCurrency.code}`;
+        } else {
+            resultDiv.textContent = "Error al realizar la conversión.";
+        }
+
+
+
     });
 
     function populateCurrencies(selectElement, currencies) {
